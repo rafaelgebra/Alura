0ac7cd8e3d8bd328b41d3e96b63fcc9475f223bd
unknown <rafaelrodriguesgebra@gmail.com>
2022-09-10 20:26:49 -0300

    Alura

--- Alura_dia_4-Aluraflix-2.js
+++ Alura_dia_4-Aluraflix-2.js
@@ -1,6 +1,6 @@
 
 
-// [ ]
+// acrescentar link da imagem dentro dos colchetes [] e separa por , 
 var listaFilmes = [  ];
   for (var i = 0; i < listaFilmes.length; i++) {
     document.write("<img src=" + listaFilmes[i] + ">");

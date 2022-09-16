3cce100595c14f43a50174c9c8af4fe87c2b6975
unknown <rafaelrodriguesgebra@gmail.com>
2022-09-16 09:59:13 -0300

    alura_dia_7-Super_trunfo_Logica_do_jogo.js

--- alura_dia_7-Super_trunfo_Logica_do_jogo.js
+++ alura_dia_7-Super_trunfo_Logica_do_jogo.js
@@ -1,45 +1,45 @@
 var carta1 = {
-    nome: "Bulbasauro",
-    atributos: {
-      ataque: 7,
-      defesa: 9,
-      magia: 6
-    }
-  };
-  var carta2 = {
-    nome: "Dart vader",
-    atributos: {
-      ataque: 9,
-      defesa: 8,
-      magia: 2
-    }
-  };
-  var carta3 = {
-    nome: "shiryu de dragao",
-    atributos: {
-      ataque: 5,
-      defesa: 9,
-      magia: 10
-    }
-  };
-  
-  var cartas = [carta1, carta2, carta3];
-  var cartaMaquina;
-  var cartaJogador;
-  
-  function sortearCarta() {
-    var numeroCartaMaquina = parseInt(Math.random() * 3);
-    cartaMaquina = cartas[numeroCartaMaquina];
-  
-    var numeroCartaJogador = parseInt(Math.random() * 3);
-    while (numeroCartaMaquina == numeroCartaJogador) {
-      numeroCartaJogador = parseInt(Math.random() * 3);
-    }
+  nome: "Bulbasauro",
+  atributos: {
+    ataque: 7,
+    defesa: 8,
+    magia: 6
+  }
+};
+var carta2 = {
+  nome: "Darth Vader",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    magia: 2
+  }
+};
+var carta3 = {
+  nome: "Shiry de dragao",
+  atributos: {
+    ataque: 5,
+    defesa: 9,
+    magia: 10
+  }
+};
+var cartas = [carta1, carta2, carta3];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 3);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 3);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * 3);
+  }
     cartaJogador = cartas[numeroCartaJogador];
     console.log(cartaJogador);
-  
+
     document.getElementById("btnSortear").disabled = true;
     document.getElementById("btnJogar").disabled = false;
+    
     exibirOpcoes();
   }
   function exibirOpcoes() {
@@ -48,7 +48,7 @@ var carta1 = {
   
     for (var atributo in cartaJogador.atributos) {
       opcoesTexto +=
-        "<input type='radio' nome='atributo' value='" +
+        "<input type='radio' name='atributo' value='" +
         atributo +
         "'>" +
         atributo;
@@ -57,6 +57,7 @@ var carta1 = {
   }
   function obtemAtributoSelecionado() {
     var radioAtributos = document.getElementsByName("atributo");
+
     for (var i = 0; i < radioAtributos.length; i++) {
       if (radioAtributos[i].checked == true) {
         return radioAtributos[i].value;
@@ -66,8 +67,8 @@ var carta1 = {
   function jogar() {
     var atributoSelecionado = obtemAtributoSelecionado();
     var elementoResultado = document.getElementById("resultado");
-    var valorCartaJogador = cartaJogador.atributo[atributoSelecionado];
-    var valorCartaMaquina = cartaMaquina.atributo[atributoSelecionado];
+    var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+    var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
   
     if (valorCartaJogador > valorCartaMaquina) {
       elementoResultado.innerHTML = "Voce venceu";
